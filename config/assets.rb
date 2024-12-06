e32082f69fd8495d09f8775180063e173a89082b
stephanie rousset <strabon@club-internet.fr>
2024-12-06 14:03:31 +0100

    feat: add css files for extend_socio_demographic_handler functionality

--- config/assets.rb
+++ config/assets.rb
@@ -4,6 +4,7 @@ base_path = File.expand_path("..", __dir__)
 
 Decidim::Webpacker.register_path("#{base_path}/app/packs")
 Decidim::Webpacker.register_entrypoints(
-  decidim_additional_authorization_handler: "#{base_path}/app/packs/entrypoints/decidim_additional_authorization_handler.js"
+  decidim_additional_authorization_handler: "#{base_path}/app/packs/entrypoints/decidim_additional_authorization_handler.js",
+  decidim_additional_authorization_handler_css: "#{base_path}/app/packs/stylesheets/decidim/additional_authorization_handler/additional_authorization_handler.scss"
 )
-Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/additional_authorization_handler/additional_authorization_handler")
+#Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/additional_authorization_handler/additional_authorization_handler")
