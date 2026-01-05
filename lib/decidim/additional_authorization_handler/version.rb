269d24c092c35a15323d5f135e91612259b92f33
stephanie rousset <strabon@club-internet.fr>
2026-01-05 17:33:13 +0100

    feat: update versions et gems

--- lib/decidim/additional_authorization_handler/version.rb
+++ lib/decidim/additional_authorization_handler/version.rb
@@ -4,11 +4,11 @@ module Decidim
   # This holds the decidim-meetings version.
   module AdditionalAuthorizationHandler
     def self.decidim_version
-      "~> 0.29.0"
+      "~> 0.31.0"
     end
 
     def self.version
-      "1.0.0"
+      "1.1.0"
     end
   end
 end
